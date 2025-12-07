8cf4e68e9b1c274d9856d5b31d97edfa0916ce8e
Manian VJS <manian.vjs@gmail.com>
2025-12-07 20:52:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
-// 👇 This is required endpoint YouTrack is calling
 app.post("/webhook", (req, res) => {
   console.log("📩 Webhook received from YouTrack:", req.body);
-
   res.status(200).send("Webhook received successfully!");
 });
 
-// Basic home route
 app.get("/", (req, res) => {
   res.send("Webhook Server Running 🚀");
 });
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`🚀 Server Live on PORT ${PORT}`);
 });
